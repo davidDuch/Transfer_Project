e47687134c6e1dc8710adda0a76c378c7944a93b
Gabi Malin <gabi939@gmail.com>
2018-12-12 13:14:16 +0200

    Update CreateAdviceScreenController.java

--- TheProject v7/View/CreateAdviceScreenController.java
+++ TheProject v7/View/CreateAdviceScreenController.java
@@ -21,7 +21,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
